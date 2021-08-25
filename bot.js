5b0491c9d81ddd5001d354c59ec2884fbec4bf27
SithuCo <87355697+SithuCo@users.noreply.github.com>
2021-08-25 17:01:25 +0630

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ instagram : adimas_shadoet
 */
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('1675749539:AAH46KNMRschhJDMuI5dclpp9FnB0znEaK4')
+const bot = new Telegraf('1996188775:AAFmyXdHeWnHI47WmjZlGJ9cVnSrhCBEA44')
 const axios = require('axios')
 
 /*********[ Apikey ]*********/
